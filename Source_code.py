0c572d2746954dcaab6feeda059a5d25117df3ce
kishorprajapati99 <90930174+kishorprajapati99@users.noreply.github.com>
2021-10-17 22:09:57 +0530

    Update Source_code.py

--- Source_code.py
+++ Source_code.py
@@ -4,4 +4,6 @@ BMI_df = pd.read_json("Source_file.json")
 # print(cars)
 cm_to_m = BMI_df['HeightCm']/100
 BMI_df["BMI"] = BMI_df['WeightKg'] / (cm_to_m ** 2)
-print(BMI_df)
+# print(BMI_df)
+filter_1 = BMI_df[BMI_df['BMI'].between(25.0, 29.9)]
+print(filter_1)
